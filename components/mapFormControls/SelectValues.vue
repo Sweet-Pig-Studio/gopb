f69f0527ee3080ac4f39765631f4b415a6b12431
cassandraGoose <cassandra.torske@gmail.com>
2024-10-10 10:55:08 -0600

    add map list toggle visually; fix small accessibility issue

--- components/mapFormControls/SelectValues.vue
+++ components/mapFormControls/SelectValues.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="form control">
-    <label class="label">
+    <label class="label" :for="selectLabel">
       <span class="label-text">{{ selectLabel }}</span>
     </label>
-    <select class="select select-bordered rounded" defaultValue="Any">
+    <select :id="selectLabel" class="select select-bordered rounded" defaultValue="Any">
       <option v-for="(selectOption, i) in selectOptions">{{  selectOption }}</option>
     </select>
   </div>
--- components/mapFormControls/index.vue
+++ components/mapFormControls/index.vue
@@ -2,12 +2,29 @@
   <form
     class="flex w-full component-preview pb-2 items-center justify-between gap-2"
   >
-    <MapFormControlsSearch/>
+    <MapFormControlsSearch />
     <div class="flex gap-2">
-      <MapFormControlsSelectValues :selectOptions="['Any', 'Restaurant', 'Grocery']" :selectLabel="'Location Type:'"/>
-      <MapFormControlsSelectValues :selectOptions="['Any', 'coming soon', 'for real', 'soon']" :selectLabel="'Cuisine Type:'" />
-      <MapFormControlsSelectValues :selectOptions="['Any', 'Fully Plant Based', 'Partially Plant Based']" :selectLabel="'Plant-Based Level:'" />
-      <div>map list vue radio here</div>
+      <MapFormControlsSelectValues
+        :selectOptions="['Any', 'Restaurant', 'Grocery']"
+        :selectLabel="'Location Type:'"
+      />
+      <MapFormControlsSelectValues
+        :selectOptions="['Any', 'coming soon', 'for real', 'soon']"
+        :selectLabel="'Cuisine Type:'"
+      />
+      <MapFormControlsSelectValues
+        :selectOptions="['Any', 'Fully Plant Based', 'Partially Plant Based']"
+        :selectLabel="'Plant-Based Level:'"
+      />
+      <fieldset class="form control">
+        <legend class="label">
+          <span class="label-text"> View as... </span>
+        </legend>
+        <div class="join">
+          <button class="btn join-item rounded">Map</button>
+          <button class="btn join-item rounded">List</button>
+        </div>
+      </fieldset>
     </div>
   </form>
 </template>
